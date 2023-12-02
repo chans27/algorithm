c303be97e64e45760a2bdd5c4ff1899d8bd3975a
YEACHAN <everever1275@gmail.com>
2023-12-02 22:32:50 +0900

    algorithm

--- src/lv1/ForCopy.java
+++ src/lv1/ForCopy.java
@@ -4,7 +4,7 @@ public class ForCopy {
     public static void main(String[] args) {
         System.out.println(solution(3));
     }
-    public static int solution(int num) {
-        return num;
+    public static int solution(int n) {
+        return n;
     }
 }

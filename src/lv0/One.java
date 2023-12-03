c1708bd6d593cab73f09b717ad97309ecbeb2f09
YEACHAN <everever1275@gmail.com>
2023-12-04 07:57:58 +0900

    algorithm

--- src/lv0/One.java
+++
@@ -1,7 +0,0 @@
-package lv0;
-
-public class One {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/lv1/Remainder.java
@@ -0,0 +1,20 @@
+package lv1;
+
+import java.util.Scanner;
+
+public class Remainder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        scanner.close();
+
+        System.out.println((a + b) % c);
+        System.out.println((a % c + b % c) % c);
+        System.out.println((a * b) % c);
+        System.out.println((a % c * b % c) % c);
+    }
+}

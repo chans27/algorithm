735081a29698716a8fa040e3d841405d0a3dbcdd
YEACHAN <everever1275@gmail.com>
2023-12-08 07:07:55 +0900

    algorithm

---
+++ src/lv2/CompareTwoNumber.java
@@ -0,0 +1,20 @@
+package lv2;
+
+import java.util.Scanner;
+
+public class CompareTwoNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        if (a > b) {
+            System.out.println(">");
+        } else if (a < b) {
+            System.out.println("<");
+        } else {
+            System.out.println("==");
+        }
+    }
+}

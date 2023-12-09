991c4159e1f2504d82abba01bd2832ac4da9a4d8
YEACHAN <everever1275@gmail.com>
2023-12-09 18:50:37 +0900

    algorithm

---
+++ src/lv2/OvenTimer.java
@@ -0,0 +1,18 @@
+package lv2;
+
+import java.util.Scanner;
+
+public class OvenTimer {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int hour = sc.nextInt();
+        int minute = sc.nextInt();
+        int cookTime = sc.nextInt();
+
+        hour = ((minute + cookTime) / 60 + hour)%24;
+        minute = (minute + cookTime) % 60;
+
+        System.out.println(hour + " " + minute);
+    }
+}
---
+++ src/lv2/ThreeDice.java
@@ -0,0 +1,29 @@
+package lv2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ThreeDice {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int[] dice = {a, b, c};
+        Arrays.sort(dice);
+        int result;
+
+        if(dice[0] == dice[1] && dice[1] == dice[2]) {
+            result = dice[0]*1000 + 10000;
+        } else if(dice[0] == dice[1] || dice[0] == dice[2] || dice[1] == dice[2]) {
+            result = dice[1]*100 + 1000;
+        } else {
+            result = dice[2]*100;
+        }
+
+        System.out.println(result);
+    }
+}
